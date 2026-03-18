1c850035a40c4174ba73042550642eb78680fb4b
maksymkundakita-spec <maksym.kunda.kita@donntu.edu.ua>
2026-03-18 18:59:40 +0200

    Update test_api_security.py

--- tests/test_api_security.py
+++ tests/test_api_security.py
@@ -2,7 +2,7 @@ import requests
 
 BASE_URL = "http://localhost:5000"
 
-def test_xss_api_protected(flask_app):
+def test_xss_api_protected():
     response = requests.get(f"{BASE_URL}/api/user/1")
 
     assert "<script>" not in response.text, "XSS в API!"
